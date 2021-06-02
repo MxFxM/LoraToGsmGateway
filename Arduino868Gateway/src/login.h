8d6814d1ff85b6c61b4b41101f7541d7f1b733f3
MxFxM <32713448+MxFxM@users.noreply.github.com>
2021-06-02 23:01:14 +0200

    initial commit

---
+++ Arduino868Gateway/src/login.h
@@ -0,0 +1,7 @@
+// PIN number if necessary
+#define PINNUMBER "0285"
+
+// APN information obrained from your network provider
+#define GPRS_APN       "pinternet.interkom.de" // replace with your GPRS APN
+#define GPRS_LOGIN     ""    // replace with your GPRS login
+#define GPRS_PASSWORD  "" // replace with your GPRS password
